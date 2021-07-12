29df3c0ba23515dcc25fdbf55fa9555884dc915b
Dhaivath Lal <dhaivathl@gmail.com>
2021-07-12 20:02:56 +0530

    v0.0.4 Dark mode Stable

--- src/components/Defenitions.jsx
+++ src/components/Defenitions.jsx
@@ -4,13 +4,13 @@ export default function Defenitions() {
     return (
         <div className="fill flex vc hc">
             <fieldset>
-                <legend>Profile</legend>
+                <legend>PROFILE</legend>
                 <b> Username : </b> dev-D <br/><br/>
                 <b> Education :</b> <br/>
                 &nbsp;&nbsp;&nbsp; <b>L.P  School :</b> St: Dominic Savio <br/>
                 &nbsp;&nbsp;&nbsp; <b>U.P School :</b> M.K.M <br/>
                 &nbsp;&nbsp;&nbsp; <b>H.S School :</b>M.K.M<br/>
-                &nbsp;&nbsp;&nbsp; <b>Degree :</b> b.p.c college piravom<br/><br/>
+                &nbsp;&nbsp;&nbsp; <b>Degree :</b> B.P.C College Piravom<br/><br/>
 
                 <b> Works on :</b> HTML, CSS, JS, Php, React, SQL.
             </fieldset>
