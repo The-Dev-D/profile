0ac96e9fef741d34094dca5c7399617006fe6396
The-Dev-D <87186143+The-Dev-D@users.noreply.github.com>
2021-07-12 16:32:39 +0530

    Update Defenitions.jsx

--- src/components/Defenitions.jsx
+++ src/components/Defenitions.jsx
@@ -10,7 +10,7 @@ export default function Defenitions() {
                 &nbsp;&nbsp;&nbsp; <b>L.P  School :</b> St: Dominic Savio <br/>
                 &nbsp;&nbsp;&nbsp; <b>U.P School :</b> M.K.M <br/>
                 &nbsp;&nbsp;&nbsp; <b>H.S School :</b>M.K.M<br/>
-                &nbsp;&nbsp;&nbsp; <b>Degree :</b> b.p.c college piravom<br/><br/>
+                &nbsp;&nbsp;&nbsp; <b>Degree :</b> B.P.C College Piravom<br/><br/>
 
                 <b> Works on :</b> HTML, CSS, JS, Php, React, SQL.
             </fieldset>
