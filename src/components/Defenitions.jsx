7c5a6a30dc2ccf30163a163be5af3410a05c1a37
Dhaivath Lal <dhaivathl@gmail.com>
2021-07-13 16:15:13 +0530

    v0.1.0 base built complete

--- src/components/Defenitions.jsx
+++ src/components/Defenitions.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 export default function Defenitions() {
     return (
         <div className="fill flex vc hc">
-            <fieldset>
-                <legend>PROFILE</legend>
+        <CSSTransition
+        in={true}
+        appear={true}
+        timeout={900}
+        classNames="fieldset">
+            <fieldset className="fieldset">
+                <legend> {"<"}PROFILE{">"} </legend>
                 <b> Username : </b> dev-D <br/><br/>
-                <b> Education :</b> <br/>
-                &nbsp;&nbsp;&nbsp; <b>L.P  School :</b> St: Dominic Savio <br/>
-                &nbsp;&nbsp;&nbsp; <b>U.P School :</b> M.K.M <br/>
-                &nbsp;&nbsp;&nbsp; <b>H.S School :</b>M.K.M<br/>
-                &nbsp;&nbsp;&nbsp; <b>Degree :</b> B.P.C College Piravom<br/><br/>
+                <b> Education :</b> <br/> <br/>
+
+                &emsp;&emsp;<b>L.P  School :</b> St: Dominic Savio <br/>
+                &emsp;&emsp;<b>U.P School :</b> M.K.M <br/>
+                &emsp;&emsp;<b>H.S School :</b>M.K.M<br/>
+                &emsp;&emsp;<b>Degree :</b> B.P.C College Piravom<br/><br/>
 
                 <b> Works on :</b> &nbsp;
-                 HTML<i class="fab fa-html5 m1"></i>,
-                 CSS<i class="fab fa-css3-alt m1"></i>,
-                 JS<i class="fab fa-js m1"></i>,
-                 Php<i class="fab fa-php m1"></i>,
-                 React<i class="fab fa-react m1"></i>, 
-                 SQL<i class="fas fa-database m1"></i>
+                HTML<i class="fab fa-html5 m1"></i>,    
+                CSS<i class="fab fa-css3-alt m1"></i>,  
+                JS<i class="fab fa-js m1"></i>,         
+                Php<i class="fab fa-php m1"></i>,       
+                React<i class="fab fa-react m1"></i>,   
+                SQL<i class="fas fa-database m1"></i>   
             </fieldset>
+        </CSSTransition>
         </div>
     )
 }
