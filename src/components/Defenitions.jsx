cfda473e5a768a5e6e724fbbcfb955eae4748bdf
Dhaivath Lal <dhaivathl@gmail.com>
2021-07-15 14:09:47 +0530

    v1.0.3 stable class, map added

--- src/components/Defenitions.jsx
+++ src/components/Defenitions.jsx
@@ -1,35 +1,33 @@
 import React from 'react';
 import { CSSTransition } from 'react-transition-group';
+import Fields from './Fields';
+import { lang } from './languages';
 
 export default function Defenitions() {
     return (
         <div className="fill flex vc hc">
-        <CSSTransition
-        in={true}
-        appear={true}
-        timeout={900}
-        classNames="fieldset">
+        <CSSTransition in={true} appear={true} timeout={900} classNames="fieldset">
+
             <fieldset className="fieldset">
                 <legend> {"<"}PROFILE{">"} </legend>
-                <b> Username : </b> dev-D <br/><br/>
+                <Fields field="Username" value="dev-D"/> <br/><br/>
                   
                 <b> Education :</b> <br/> <br/>
 
-                <b> Education :</b> <br/>
+                &emsp;&emsp;<Fields field="L.P School" value="St: Dominic Savio"/> <br/>
+                &emsp;&emsp;<Fields field="U.P School" value="M.K.M" /> <br/>
+                &emsp;&emsp;<Fields field="H.S School" value="M.K.M" /> <br/>
+                &emsp;&emsp;<Fields field="Degree" value="B.P.C College Piravom" /> <br/>
+                <br/>
+                <b>Works on</b> : 
+                {
+                    lang.map((value,index) => {
+                        return <Fields icon={value.icon} value={value.name}/>
+                    })
+                }
 
-                &emsp;&emsp;<b>L.P  School :</b> St: Dominic Savio <br/>
-                &emsp;&emsp;<b>U.P School :</b> M.K.M <br/>
-                &emsp;&emsp;<b>H.S School :</b>M.K.M<br/>
-                &emsp;&emsp;<b>Degree :</b> B.P.C College Piravom<br/><br/>
-
-                <b> Works on :</b> &nbsp;
-                HTML<i class="fab fa-html5 m1"></i>,    
-                CSS<i class="fab fa-css3-alt m1"></i>,  
-                JS<i class="fab fa-js m1"></i>,         
-                Php<i class="fab fa-php m1"></i>,       
-                React<i class="fab fa-react m1"></i>,   
-                SQL<i class="fas fa-database m1"></i>   
             </fieldset>
+            
         </CSSTransition>
         </div>
     )
