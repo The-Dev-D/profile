7fc9bfbdde13a4110f2730a0c2c7e70b297a00b4
the-dev-d <dhaivathl@gmail.com>
2021-08-31 22:49:52 +0530

    new picture frame and colors

--- src/components/Defenitions.jsx
+++ src/components/Defenitions.jsx
@@ -15,7 +15,7 @@ export default function Defenitions(props) {
         <CSSTransition in={props.appearState} appear={false} timeout={900} classNames="fieldset">
 
             <fieldset className="fieldset">
-                <legend align="center"> {"<"}Personal Info{">"} </legend>
+                <legend align="center"> {"<"}PERSONAL INFO{">"} </legend>
                 <Fields field="Username" value="dev-D"/> <br/><br/>
                 <Fields field="D.O.B" value="25/05/2002" /><br/><br/>
                 <b> Education :</b> <br/> <br/>
@@ -28,7 +28,7 @@ export default function Defenitions(props) {
                 <b>Works on</b> : 
                 {
                     lang.map((value,index) => {
-                        return <> <Fields key={index} icon={value.icon} value={value.name}/></>
+                        return <> <Fields key={index} icon={value.icon} value={value.name} color={value.color}/></>
                     })
                 }
 
